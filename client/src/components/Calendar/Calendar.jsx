2fbc2fbfbd8b7fa2e021f307ad269617273cc831
Kostyantyn Shvydkyi <72373801+kshvydkyi@users.noreply.github.com>
2022-11-10 17:50:58 +0200

    Артем, досить займатись сексом, час робити aut

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import '../../css/Calendar.css'
-import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
+import CalendarComp from "@ericz1803/react-google-calendar"
+
+const API_KEY = "AIzaSyBmzeX-SoCjmUgrPhdM26nDZ2biIkHukCA";
+let calendars = [
+  {calendarId: "09opmkrjova8h5k5k46fedmo88@group.calendar.google.com", color: "#B241D1"}, //add a color field to specify the color of a calendar
+  {calendarId: "hkr1dj9k6v6pa79gvpv03eapeg@group.calendar.google.com"}, //without a specified color, it defaults to blue (#4786ff)
+  {calendarId: "rg4m0k607609r2jmdr97sjvjus@group.calendar.google.com", color: "rgb(63, 191, 63)"} //accepts hex and rgb strings (doesn't work with color names)
+];
 
 const Calendar = () => { 
-    const dateValue = new Date(new Date().getFullYear(), new Date().getMonth(), 10);
-    const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 6);
-    const maxDate = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
+    
     return(
     <>
     <div className="form-background d-flex justify-content-center text-white">
         <div className='d-flex flex-column'>
-        <h1 className="text-white">КАЛЕНДАР</h1>
-        <CalendarComponent 
-        value={dateValue} 
-        min={minDate} 
-        max={maxDate}
-        isMultiSelection={true}
-        start="Decade"
-        depth="Year"
-        ></CalendarComponent>
+        <CalendarComp apiKey={API_KEY} calendars={calendars} />
         </div>
         </div>
     </>
