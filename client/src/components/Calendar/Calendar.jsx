444a8cd2289d446d9f02a46a1813e940178a664a
Kostyantyn Shvydkyi <72373801+kshvydkyi@users.noreply.github.com>
2022-11-10 16:36:19 +0200

    react-calendar

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,12 +1,25 @@
-const Calendar = () => {
+import React from 'react'
+import '../../css/Calendar.css'
+import {CalendarComponent} from '@syncfusion/ej2-react-calendars';
 
-
-
-    
- return(
+const Calendar = () => { 
+    const dateValue = new Date(new Date().getFullYear(), new Date().getMonth(), 10);
+    const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 6);
+    const maxDate = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
+    return(
     <>
     <div className="form-background d-flex justify-content-center text-white">
+        <div className='d-flex flex-column'>
         <h1 className="text-white">КАЛЕНДАР</h1>
+        <CalendarComponent 
+        value={dateValue} 
+        min={minDate} 
+        max={maxDate}
+        isMultiSelection={true}
+        start="Decade"
+        depth="Year"
+        ></CalendarComponent>
+        </div>
         </div>
     </>
  )
